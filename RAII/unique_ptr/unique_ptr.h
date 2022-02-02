26c0ad1e0ad8603418ded340706c0deb6645952f
Hrach <86060764+Hrach-Harutyunyan@users.noreply.github.com>
2022-02-03 03:02:53 +0400

    Update unique_ptr.h

--- RAII/unique_ptr/unique_ptr.h
+++ RAII/unique_ptr/unique_ptr.h
@@ -1,6 +1,3 @@
-#ifndef _IMPLEMENTATION_HPP
-#define _IMPLEMENTATION_HPP
-
 #include <iostream>
 
 template <class T>
@@ -32,7 +29,66 @@ private:
     T* ptr;
 };
 
-#include "implementation.hpp"
+template <class T>
+unique_ptr<T>::unique_ptr() : ptr(nullptr) {};
 
-#endif
 
+template <class T>
+unique_ptr<T>::unique_ptr(T* pointer) : ptr(pointer) {} ;
+
+template <class T>
+unique_ptr<T>::unique_ptr(unique_ptr<T>&& rhs) noexcept : ptr(nullptr) {
+    rhs.swap(*this);
+};
+
+template <class T>
+unique_ptr<T>& unique_ptr<T>::operator = (unique_ptr<T>&& rhs) noexcept {
+    this->swap(rhs);
+    delete rhs.ptr;
+    rhs.ptr = nullptr;
+    return *this;
+};
+
+template <class T>
+unique_ptr<T>::~unique_ptr() {
+    if(!ptr){
+        delete ptr;
+    }
+};
+
+template <class T>
+T& unique_ptr<T>::operator *() const {
+    return *ptr;
+};
+
+template <class T>
+T* unique_ptr<T>::operator ->() const {
+    return ptr;
+};
+
+template <class T>
+unique_ptr<T>::operator bool() const {
+    return (ptr == nullptr? false:true);
+};
+
+
+template <class T>
+void unique_ptr<T>::swap(unique_ptr<T>& other) noexcept {
+    std::swap(ptr, other.ptr);
+}
+
+template <class T>
+T* unique_ptr<T>::get() {
+    return ptr;
+}
+
+template <class T>
+void unique_ptr<T>::reset() {
+    delete this->ptr;
+    this->ptr = nullptr;
+}
+
+template <class T>
+void unique_ptr<T>::release() {
+    this->ptr = nullptr;
+}
