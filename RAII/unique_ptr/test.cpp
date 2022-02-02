0ed0def95131b423b60088d0cdf7b513b2752cca
Hrach <86060764+Hrach-Harutyunyan@users.noreply.github.com>
2022-02-03 03:04:30 +0400

    Update test.cpp

--- RAII/unique_ptr/test.cpp
+++ RAII/unique_ptr/test.cpp
@@ -1,12 +1,6 @@
 #include "unique_ptr.h"
 
 int main() {
-
     
-    int *a = new int{2222};
-    unique_ptr <int> ptr(a);
-    std::cout << "a = " << *a <<std::endl;
-    std::cout << "ptr = " << *ptr << std::endl;
-
     return 0;
 }
